9431420b39c9d830667b34211ace13f68b5793a2
Wesley Lacerda <46899059+LumaCorp-web@users.noreply.github.com>
2026-08-19 22:08:29 -0300

    Bump Electron loading module revision

--- app/public/js/modules/room-service-erp/app.js
+++ app/public/js/modules/room-service-erp/app.js
@@ -1,6 +1,6 @@
 import { setupDesktopBillingFilters } from "./desktop-billing-filters.js?v=20260819-3";
 import { setupDesktopControls } from "./desktop-adapter.js?v=20260814-6";
-import { setupDesktopLoadingExperience } from "./desktop-loading-experience.js?v=20260819-1";
+import { setupDesktopLoadingExperience } from "./desktop-loading-experience.js?v=20260819-2";
 import { setupIconSystem } from "./icon-system.js?v=20260814-6";
 import { setupSidebarAccount } from "./sidebar-account.js?v=20260819-3";
 import "./legacy-app.js?v=20260814-6";
