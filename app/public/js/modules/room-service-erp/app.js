d1a25d4b0f5e715230384480a4845eef4ec43b85
Wesley Lacerda <46899059+LumaCorp-web@users.noreply.github.com>
2026-08-19 22:22:10 -0300

    Refresh Electron loading module import

--- app/public/js/modules/room-service-erp/app.js
+++ app/public/js/modules/room-service-erp/app.js
@@ -1,6 +1,6 @@
 import { setupDesktopBillingFilters } from "./desktop-billing-filters.js?v=20260819-3";
 import { setupDesktopControls } from "./desktop-adapter.js?v=20260814-6";
-import { setupDesktopLoadingExperience } from "./desktop-loading-experience.js?v=20260819-2";
+import { setupDesktopLoadingExperience } from "./desktop-loading-experience.js?v=20260819-3";
 import { setupIconSystem } from "./icon-system.js?v=20260814-6";
 import { setupSidebarAccount } from "./sidebar-account.js?v=20260819-4";
 import "./legacy-app.js?v=20260814-6";
