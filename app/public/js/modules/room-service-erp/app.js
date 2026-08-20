9f0a4cba4078c7fb4d5e879bda3db951669292d5
Wesley Lacerda <46899059+LumaCorp-web@users.noreply.github.com>
2026-08-19 21:33:56 -0300

    Wire Electron ERP loading experience

--- app/public/js/modules/room-service-erp/app.js
+++ app/public/js/modules/room-service-erp/app.js
@@ -1,8 +1,10 @@
 import { setupDesktopControls } from "./desktop-adapter.js?v=20260814-6";
+import { setupDesktopLoadingExperience } from "./desktop-loading-experience.js?v=20260819-1";
 import { setupIconSystem } from "./icon-system.js?v=20260814-6";
 import { setupSidebarAccount } from "./sidebar-account.js?v=20260819-2";
 import "./legacy-app.js?v=20260814-6";
 
 setupIconSystem();
 setupDesktopControls();
+setupDesktopLoadingExperience();
 setupSidebarAccount();
