ed71a276732a8ed3699fb35ea106be83860e3191
Wesley Lacerda <46899059+LumaCorp-web@users.noreply.github.com>
2026-08-19 21:26:14 -0300

    Bust Electron account module cache

--- app/public/js/modules/room-service-erp/app.js
+++ app/public/js/modules/room-service-erp/app.js
@@ -1,6 +1,6 @@
 import { setupDesktopControls } from "./desktop-adapter.js?v=20260814-6";
 import { setupIconSystem } from "./icon-system.js?v=20260814-6";
-import { setupSidebarAccount } from "./sidebar-account.js?v=20260819-1";
+import { setupSidebarAccount } from "./sidebar-account.js?v=20260819-2";
 import "./legacy-app.js?v=20260814-6";
 
 setupIconSystem();
